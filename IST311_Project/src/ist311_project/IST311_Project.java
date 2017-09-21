025de89212d87abe5827a6bf0c7e1003498ebe46
Nate Cox <Nate Cox@blackBeast>
2017-09-20 20:09:15 -0400

    Initialize IST 311 Final Project

---
+++ IST311_Project/src/ist311_project/IST311_Project.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ist311_project;
+
+/**
+ *
+ * @author Nate Cox
+ */
+public class IST311_Project {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
